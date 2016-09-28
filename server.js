fbe927f669696ce0279e9329b31cfb55817bbc80
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-09-28 23:17:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,6 +25,12 @@ var articleOne={
     
 };
 
+function createTemplate(data){
+
+var title=data.title;
+var date =data.date;
+var content=data.content;
+var heading=data.heading; 
 
 var htmlTemplate=`<html>
 <head>
@@ -69,6 +75,10 @@ var htmlTemplate=`<html>
 </html>
 `;
 
+return htmlTemplate;
+
+}
+
 
 
 app.get('/', function (req, res) {
@@ -81,7 +91,7 @@ app.get('/ui/style.css', function (req, res) {
 
 
 app.get('/article-one' ,function(req , res){
-	res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+	res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req , res){
