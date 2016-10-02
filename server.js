9f4c83ad7ea9959bcb804286bd4d9e1933314f94
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-10-02 23:05:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -151,6 +151,13 @@ app.get('/article-ten', function (req , res){
 	res.send('Article nine will be served here');
 	
 } );
+
+app.get('/ui/main.js', function (req , res){
+	res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+	
+} );
+
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
