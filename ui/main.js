fb599d8947cdc7d0b7a487d2231ffcfbe4a74171
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-10-04 21:46:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 
 var element =document.getElementById("mainId");
 element.innerHTML="This is the new Value of the Element";
+
+var img =document.getElementById('shiva');
+img.onclick=function() {
+    
+    img.style.marginLeft="100px";
+}
