75d199f0062061afa999ccfc84abc144c5119737
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-10-06 21:59:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
  //counter code
- var button =document.getElementById("counter");
+ var button =document.getElementById('counter');
 
  button.onclick=function(){
      
@@ -12,14 +12,14 @@
             // Take some action  
             if(request.status===200){
                 var counter =request.responseText;
-                 var span =document.getElementById("count");
+                 var span =document.getElementById('count');
                   span.innerHTML=counter.toString();
             }
          }
          
      };
      //make a request 
-     request.open("GET","http://gaurav-maskara.imad.hasura-app.io/counter",true);
+     request.open('GET','http://gaurav-maskara.imad.hasura-app.io/counter',true);
      request.send(null);
      
  };
