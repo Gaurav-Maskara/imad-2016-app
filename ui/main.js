bb5547d7747581ea58bcaeb78173e087d47ec6fa
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-10-07 23:39:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@
      var names =["name1","name2","name3"];
      var list="";
      for(var i=0;i<names.length;i++){
-         list="<li>"+names[i]+"</li>";
+         list=list+"<li>"+names[i]+"</li>";
      }
      
      var ul =document.getElementById("nameList");
