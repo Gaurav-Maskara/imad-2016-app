d5467ffb67f229e916b7df8caaeb54349a378301
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-10-08 13:41:25 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -63,30 +63,7 @@
  // to add comments to the three article pages
  
  
- var commentSubmitButton=document.getElementById("submitComment");
- commentSubmitButton.onclick=function(){
-     
-      var request=new XMLHttpRequest();
-     //capture the response and store it in a variable
-     request.onreadystatechange=function(){
-         if(request.readyState==XMLHttpRequest.DONE){
-            // Take some action  
-            if(request.status==200){
-                var comment =request.responseText;
-                comment=JSON.parse(comment);
-                document.getElementById("comments").innerHTML=comment;
-                
-             }
-          }
-         
-     };
-     
-     var comment=document.getElementById("textBox");
-     var commentValue=comment.value;
-     request.open("GET","http://gaurav-maskara.imad.hasura-app.io/submit-comment?comment="+commentValue,true);
-     request.send(null);
-     
- };
+
  
  
  
