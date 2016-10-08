46276aa81709bf3b4393e7660d9321139f5f2978
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-10-08 13:15:21 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -146,6 +146,15 @@ app.get("/submit-name",function(req ,res){ //URL :/submit-name?name=xxxxx
     
 });
 
+
+
+app.get("/submit-comment",function(req ,res){ 
+    //get the name from the request object 
+    var comment=req.query.comment;
+    res.send(JSON.stringify(comment));
+    
+});
+
 app.get('/:articleName' ,function(req, res){
     // articleName==article-one
     
