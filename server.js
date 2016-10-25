cf95b18636fbe0803759ada43071c06be3b595e7
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-10-25 19:06:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -228,6 +228,15 @@ app.get('/img/go.png', function (req, res) {
 });
 
 
+app.get('/font/font/fontello.ttf', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/font/font/', 'fontello.ttf'));
+});
+
+app.get('/font/font/fontello.woff', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/font/font/', 'fontello.woff'));
+});
+
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
