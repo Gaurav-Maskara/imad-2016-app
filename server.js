29d3afdb4c7b0995f2c28bf4759765af7ee49a6d
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-10-28 16:30:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config={
     database:'gaurav-maskara',
     db:'db.imad.hasura-app.io',
     port:'5432',
-    password:'process.env.DB_PASSWORD'
+    password:process.env.DB_PASSWORD
 };
 var app = express();
 app.use(morgan('combined'));
