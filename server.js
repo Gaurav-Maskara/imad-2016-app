b2e0b401f0757626f14d2320ecd3263d050c8474
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-10-28 16:02:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
 
+var config={
+    user:'gaurav-maskara',
+    database:'gaurav-maskara',
+    db:'db.imad.hasura-app.io',
+    port:'5432',
+    password:'process.env.DB_PASSWORD'
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -121,6 +129,21 @@ var htmlTemplate=`<html>
 return htmlTemplate;
 
 }
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+   //make a select request 
+   // return a response with the results
+   pool.query('SELECT * FROM test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());  
+       }
+       else{
+           res.send(JSON.stringfy(result));
+       }
+       
+   });
+});
 
 
 
