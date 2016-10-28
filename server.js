e01fa5269793af96403d620136089b8aff7c509a
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-10-29 00:13:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,56 +69,86 @@ var date =data.date;
 var content=data.content;
 var heading=data.heading; 
 
-var htmlTemplate=`<html>
+var htmlTemplate=`
+<html lang="en">
 <head>
-    
-    <title>
-    ${title}
-    </title>
-    
-    <meta name="viewport" content="width=device-width ,initial-scale=1">
-    <link href="/ui/style.css" rel="stylesheet" />
-    <script type="text/javascript" src="/ui/article.js"></script>
-   
+<title>
+ ${title}
+</title>
+<link rel="shortcut icon" href="http://i-cdn.phonearena.com/images/article/58161-image/25-cool-new-icon-packs-and-themes-to-freshen-up-your-Android-style-2014-edition.jpg" type="image/x-icon" />
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="font/css/fontello.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css" media="screen">
 </head>
-    
-    <body>
-        
-        <div class="container">
-        
-        
-           <div>
-            <a href="/">Home</a>
-           </div>
-        
-           <hr/>
-        
-            <h3>
-               ${heading}
-            </h3>
-        
-            <div>
-               ${date.toDateString()}
-            </div>
-        
-        
-        <div>
-        ${content}
-        </div>
-      
-      
-      
-        <div id="footer" class="footer">
-        <input id="textBox" type="textbox" placeholder="Enter your comments here "> 
-        <input id="submitComment" type="submit" onclick="myfunction()">
-        
-        
-        <span id="comments"></span>
-       
-       </div>
-        
-    </body>
-    
+<body>
+<div class="navbar">
+  <div class="navbar-inner">
+    <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a> <a class="brand" href="index.html"><img src="img/user.jpg" alt=""></a>
+      <ul class="nav nav-collapse pull-right">
+        <li><a href="index.html"><i class="icon-user"></i> Profile</a></li>
+        <li><a href="skills.html"><i class="icon-trophy"></i> Skills</a></li>
+        <li><a href="work.html" class="active"><i class="icon-picture"></i> Articles</a></li>
+        <li><a href="resume.html"><i class="icon-doc-text"></i> Resume</a></li>
+      </ul>
+        <ul class="nav nav-collapse pull-right">
+        <li><a href="index.html" class="active"><i class="icon-doc-text"></i> Login/SignUp</a></li>
+      </ul>
+      <div class="nav-collapse collapse"></div>
+    </div>
+  </div>
+</div>
+<div class="container work">
+  <h2> ${heading}</h2>
+  <div>
+  ${date.toDateString()}
+  </div>
+  
+  <div>
+   ${content}
+  </div>
+  
+  
+</div>
+<div class="row social">
+  <ul class="social-icons">
+    <li><a href="#"><img src="img/fb.png" alt=""></a></li>
+    <li><a href="https://www.linkedin.com/in/gaurav-maskara-8682a0a6?trk=hp-identity-name"><img src="img/linkedin_circle_color-512.png" alt=""></a></li>
+    <li><a href="#"><img src="img/tw.png" alt=""></a></li>
+    <li><a href="#"><img src="img/go.png" alt=""></a></li>
+  </ul>
+</div>
+<div class="footer">
+  <div class="container">
+    <p class="pull-left"><a href="https://mail.google.com/mail/?view=cm&fs=1&to=gauravmaskara007@gmail.com">gauravmaskara007@gmail.com</a></p>
+    <p class="pull-right"><a href="#myModal" role="button" data-toggle="modal"> <i class="icon-mail"></i> CONTACT</a></p>
+  </div>
+</div>
+<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    <h3 id="myModalLabel"><i class="icon-mail"></i> Contact Me</h3>
+  </div>
+  <div class="modal-body">
+    <form action="#">
+      <input type="text" placeholder="Yopur Name">
+      <input type="text" placeholder="Your Email">
+      <input type="text" placeholder="Website (Optional)">
+      <textarea rows="3" style="width:80%"></textarea>
+      <br>
+      <button type="submit" class="btn btn-large"><i class="icon-paper-plane"></i> SUBMIT</button>
+    </form>
+  </div>
+</div>
+<script src="js/jquery-1.10.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script>
+$('#myModal').modal('hidden')
+</script>
+</body>
 </html>
 `;
 
