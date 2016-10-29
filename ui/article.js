980706cb6da5284ec5aceedb0d30bf7c5112820f
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-10-29 22:34:00 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,7 +1,7 @@
 // to add comment box section into the articles page
  
  
- var commentSubmitButton=document.getElementById("submitComment");
+/* var commentSubmitButton=document.getElementById("submitComment");
  function myfunction(){
      
       var request=new XMLHttpRequest();
@@ -12,6 +12,38 @@
             if(request.status==200){
                 var comment =request.responseText;
                 document.getElementById("comment").innerHTML=comment;
+                
+             }
+          }
+         
+     };
+     
+     var comment=document.getElementById("textBox");
+     var commentValue=comment.value;
+     request.open('GET','http://gaurav-maskara.imad.hasura-app.io/submit-comment?comment='+commentValue   ,true);
+     request.send(null);
+     
+ };*/
+ 
+ 
+ 
+ var commentSubmitButton=document.getElementById("submitComment");
+ function myfunction(){
+     
+      var request=new XMLHttpRequest();
+     //capture the response and store it in a variable
+     request.onreadystatechange=function(){
+         if(request.readyState==XMLHttpRequest.DONE){
+            // Take some action  
+            if(request.status==200){
+              var comments = request.responseText;
+              comments = JSON.parse(comments);
+              var list = '';
+              for (var i=0; i< comments.length; i++) {
+                  list += '<li>' + comments[i] + '</li>';
+              }
+              var ul = document.getElementById('commentList');
+              ul.innerHTML = list; 
                 
              }
           }
