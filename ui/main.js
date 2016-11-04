b4c4b83e1bb371df94ee0b8b5d2ce3b0c0263648
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-11-04 18:44:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -63,8 +63,6 @@
   function feedback(){
       
       document.getElementById("myModal").setAttribute("class", "modal hide fade");
-       document.getElementById("myModal").setAttribute("aria-hidden", "true");
-        document.getElementById("myModal").setAttribute("style", "display: none;");
         var request=new XMLHttpRequest();
         request.onreadystatechange=function(){
          if(request.readyState==XMLHttpRequest.DONE){
