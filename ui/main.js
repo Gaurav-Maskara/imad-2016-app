206aca7ea66f106e62fd6b98d754077fc6486094
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-11-06 16:33:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -86,6 +86,11 @@
      request.send(null);
  };
  
+ function getip(json){
+      alert(json.ip); // alerts the ip address
+ }
+ 
+ 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
