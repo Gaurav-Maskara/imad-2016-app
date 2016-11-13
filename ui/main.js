2d01cbb7e071e20c7aa9e3b05f6cd1dddff30975
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-11-13 17:33:03 +0530

    updated

--- ui/main.js
+++ ui/main.js
@@ -143,39 +143,42 @@ function save(location){
  
  
 // submit username and password to login into the app
- var submit_login =document.getElementById("submit_login");
- submit_login.onclick=function(){
-      //Create a request object 
-       
-     var request=new XMLHttpRequest();
-     
-     //capture the response and store it in a variable
-     request.onreadystatechange=function(){
-         if(request.readyState==XMLHttpRequest.DONE){
-            // Take some action  
-            if(request.status===200){
-               console.log("user logged in ");
-             }
-             else if(request.status===403){
-                 
-             }
-             else if(request.status===500){
-                 
-             }
-            
-          }
-         
-     };
-       var username=document.getElementById("username").value;
-       var password=document.getElementById("password").value;
-     //make a request 
-     request.open('POST','http://gaurav-maskara.imad.hasura-app.io/login', true);
-     request.setRequestHeader('Content-Type','application/json');
-     request.send(JSON.stringify({username: username, password: password}));
-     
-    
-     
- };
+ var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Success!!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials,Try again';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+};
  
  
  
