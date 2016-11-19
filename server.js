8120e40de4a8e54202e1121d2f60236b6e65f1b8
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-11-19 15:33:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -425,7 +425,7 @@ app.get('/resume/Gaurav_Maskara_Resume', function (req, res) {
 app.get('/get-comments/:articleName', function (req, res) {
    // make a select request
    // return a response with the results
-   pool.query('SELECT comment.*, "user".username FROM article, newcomment, "user" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
+   pool.query('SELECT comment.*, "user".username FROM article, newcomment, "user" WHERE article.title = $1 AND article.id = newcomment.article_id AND newcomment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
