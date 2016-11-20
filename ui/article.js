33072089f94f73f24d64e0b2026fed11bda71332
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-11-20 15:03:50 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -129,6 +129,7 @@ function loadComments () {
                 alert("response received from the server");
                 var content = '';
                 var commentsData = JSON.parse(request.responseText);
+                alert(commentsData);
                 for (var i=0; i< commentsData.length; i++) {
                     var time = new Date(commentsData[i].timestamp);
                     content += `<div class="comment">
