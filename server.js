67e8ee55a7ea5b60d582241138d7df7b4487afe7
Gaurav-Maskara <gauravmaskara007@gmail.com>
2016-11-25 13:28:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -206,13 +206,13 @@ function hash (input, salt) {
 }
 
 function escapeHtml(unsafe) {
-   /* return unsafe
+    return unsafe
          .replace(/&/g, "&amp;")
          .replace(/</g, "&lt;")
          .replace(/>/g, "&gt;")
          .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");*/
-         return unsafe;
+         .replace(/'/g, "&#039;");
+        
 }
 
 app.get('/hash/:input',function(req,res){
